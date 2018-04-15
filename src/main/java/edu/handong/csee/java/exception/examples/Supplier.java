eb5de0589ac43e49d8fca31204a591f49d98e5f6
JC <jc@lifove.net>
2018-04-15 14:53:39 +0900

    Update supplier

--- src/main/java/edu/handong/csee/java/exception/examples/Supplier.java
+++ src/main/java/edu/handong/csee/java/exception/examples/Supplier.java
@@ -1,10 +1,37 @@
 package edu.handong.csee.java.exception.examples;
 
+import java.util.Scanner;
+
 public class Supplier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Supplier mySupplier = new Supplier();
+		mySupplier.run();
+	}
 
+	private void run() {
+		Scanner keyboard = new Scanner (System.in);
+		
+		try {	
+			System.out.println ("Enter number of donuts:");
+			int donutCount = keyboard.nextInt ();
+			System.out.println ("Enter number of glasses of milk:");
+			int milkCount = keyboard.nextInt ();
+			if (milkCount < 1) {
+				keyboard.close(); // Exception means stopping the rest of statements!! 
+				throw new Exception ("Exception: No milk!");
+			}
+			double donutsPerGlass = donutCount / (double) milkCount;
+			System.out.println (donutCount + " donuts.");
+			System.out.println (milkCount + " glasses of milk.");
+			System.out.println ("You have " + donutsPerGlass +
+					" donuts for each glass of milk.");
+		} catch (Exception e) {
+			System.out.println (e.getMessage ());
+			System.out.println ("Go buy some milk.");
+		}
+		System.out.println ("End of program.");
+		keyboard.close();
 	}
 
 }
