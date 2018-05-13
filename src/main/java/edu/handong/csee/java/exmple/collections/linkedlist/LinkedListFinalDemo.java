dd8cb709eac13cf9c397d5abf6d2aea73d7f75ec
JC <JC jc@lifove.net>
2018-05-13 15:34:42 +0900

    Add LinkedListFinalDemo

---
+++ src/main/java/edu/handong/csee/java/exmple/collections/linkedlist/LinkedListFinalDemo.java
@@ -0,0 +1,24 @@
+package edu.handong.csee.java.exmple.collections.linkedlist;
+
+public class LinkedListFinalDemo {
+
+	public static void main(String[] args) {
+		LinkedListFinalDemo demonstrator = new LinkedListFinalDemo();
+		demonstrator.run();
+	}
+
+	private void run() {
+		LinkedList<String> strList = new LinkedList<String>();
+		strList.addANodeToStart("Hello");
+		strList.addANodeToStart("Good-bye");
+		strList.showList();
+		
+		
+		LinkedList<Integer> numberList = new LinkedList<Integer>();
+		for(int i = 0; i < 5 ; i++) {
+			numberList.addANodeToStart(i);
+		}
+		numberList.deleteHeadNode();
+		numberList.showList();
+	}
+}
