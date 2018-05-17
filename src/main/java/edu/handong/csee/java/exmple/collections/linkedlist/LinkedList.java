0a0e2fc806581d87baec77633a5b542bbd6fc6cd
JC <JC jc@lifove.net>
2018-05-17 10:44:48 +0900

    Updated LinkedList

--- src/main/java/edu/handong/csee/java/exmple/collections/linkedlist/LinkedList.java
+++ src/main/java/edu/handong/csee/java/exmple/collections/linkedlist/LinkedList.java
@@ -2,8 +2,9 @@ package edu.handong.csee.java.exmple.collections.linkedlist;
 
 import java.util.ArrayList;
 
-public class LinkedList<E> {
-private ListNode head;
+public class LinkedList<D> {
+	private ListNode head;
+	private ListNode tail;
 	
 	public LinkedList() {
 		head = null;
@@ -27,28 +28,65 @@ private ListNode head;
 		return count;
 	}
 	
-	public void addANodeToStart(E addData) {
+	public void addANodeToStart(D addData) {
+		if(length()<2)
+			tail = head;
 		head = new ListNode(addData,head);
 	}
 	
 	public void deleteHeadNode() {
-		if(head != null)
+		if(head != null) {
 			head = head.link;
+			if(head==null)
+				tail=null;
+		}	
 		else {
 			System.out.println("Deleting from an empty list.");
 			System.exit(0);
 		}
 	}
 	
-	public boolean onList(E target) {
+	public void addANodeToTail(D addData) {
+		if(length()==0) {
+			tail=new ListNode(addData,tail);
+			head=tail;
+		}else {
+			tail.link = new ListNode(addData,tail.link);
+		}
+	}
+	
+	public void deleteTailNode() {
+		// Get previous node
+		ListNode previous = getPreviousNode();
+		
+		// assing previous node to tail and 
+		// make the previous node link to null.
+		tail = previous;
+		previous.link = null;
+		
+	}
+	
+	private ListNode getPreviousNode() {
+		ListNode current = head;
+		ListNode previous = null;
+		while(true) {
+			if(current==tail)
+				return previous;
+			
+			previous = current; // set previons with current at it will be changed into the next node.
+			current = current.link; // move to next
+		}
+	}
+
+	public boolean onList(D target) {
 		return find(target) != null;
 	}
 	
-	private ListNode find(E target) {
+	private ListNode find(D target) {
 		boolean found = false;
 		ListNode position = head;
 		while((position != null) && !found) {
-			E dataAtPosition = position.data;
+			D dataAtPosition = position.data;
 			if(dataAtPosition.equals(target))
 				found = true;
 			else
@@ -57,8 +95,8 @@ private ListNode head;
 		return position;
 	}
 	
-	public ArrayList<E> toArrayList() {
-		ArrayList<E> list = new ArrayList<E>(length());
+	public ArrayList<D> toArrayList() {
+		ArrayList<D> list = new ArrayList<D>(length());
 		ListNode position = head;
 		while(position != null) {
 			list.add(position.data);
@@ -68,9 +106,9 @@ private ListNode head;
 	}
 	
 	private class ListNode{
-		private E data;
+		private D data;
 		
-		public E getData() {
+		public D getData() {
 			return data;
 		}
 
@@ -80,7 +118,7 @@ private ListNode head;
 
 		private ListNode link;
 		
-		public ListNode(E newData, ListNode linkedNode) {
+		public ListNode(D newData, ListNode linkedNode) {
 			data = newData;
 			link = linkedNode;
 		}
--- src/main/java/edu/handong/csee/java/exmple/collections/linkedlist/LinkedListFinalDemo.java
+++ src/main/java/edu/handong/csee/java/exmple/collections/linkedlist/LinkedListFinalDemo.java
@@ -19,6 +19,8 @@ public class LinkedListFinalDemo {
 			numberList.addANodeToStart(i);
 		}
 		numberList.deleteHeadNode();
+		numberList.deleteTailNode();
+		numberList.addANodeToTail(3);
 		numberList.showList();
 	}
 }
