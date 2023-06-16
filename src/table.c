b4a5f01c0f2b66a95e62a160b321bbea94135292
s21800612 <https://github.com/isaaclkh/manager2019>
2023-06-17 01:24:48 +0900

    UI (table) update

--- src/table.c
+++ src/table.c
@@ -4,17 +4,22 @@ void reversiSetting(int y1, int x1, int y2, int x2, int row, int col)
 {
     int w = x2 - x1;
     int h = y2 - y1;
+    char colname[]="  A    B    C    D    E    F    G    H";
+    char rowname[]={'1', '2', '3', '4', '5', '6', '7', '8'};
 
     mvprintw(y1 / 2-1, x1 / 2+3, "**** WELCOME TO REVERSI ****");
     mvprintw(y1 / 2, x1 / 2+3, "opponent has been matched");
-    mvprintw(y1 / 2+1, x1 / 2+3, "You : black stone");
-    mvprintw(y1 / 2+2, x1 / 2+3, "Opponent : white stone");
+    mvprintw(y1 / 2+2, x1 / 2+3,"%s", colname);
 
     mvhline(y1, x1, ACS_HLINE, w); // first horizontal line
     mvhline(y2, x1, ACS_HLINE, w); // second horizontal line
 
     mvvline(y1, x1, ACS_VLINE, h); // first vertical line
     mvvline(y1, x2, ACS_VLINE, h); // second vertical line
+
+    for(int i=0; i<8; i++){
+        mvprintw(y1 + (h / 8 * i) + 1, x1 / 2+1, "%c", rowname[i]);
+    }
 
     for (int i = 1; i <= 8; i++)
     {
